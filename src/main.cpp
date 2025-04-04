fe6df78d2c0392b280ae16c8f5aa5d0ca67db9d9
David <david@matterandform.net>
2025-04-05 00:38:41 +0200

    Also compare with OpenMP ; Multi scale bench

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <opencv2/opencv.hpp>
 #include <chrono>
+#include <omp.h>
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
@@ -10,6 +11,13 @@
 #include "Shader.hpp"
 #include "FrameBuffer.hpp"
 #include "Quad.hpp"
+
+typedef std::chrono::milliseconds ms;
+
+inline std::chrono::milliseconds toMS(std::chrono::high_resolution_clock::duration d)
+{
+    return std::chrono::duration_cast<std::chrono::milliseconds>(d);
+}
 
 GLFWwindow *CreateWindow()
 {
@@ -31,7 +39,7 @@ GLFWwindow *CreateWindow()
     return window;
 }
 
-void FilterCPU(const cv::Mat &input, cv::Mat &output)
+void FilterCPU(const cv::Mat &input, cv::Mat &output, bool useParallel)
 {
     CV_Assert(input.channels() == 3); // Ensure RGB
 
@@ -45,6 +53,14 @@ void FilterCPU(const cv::Mat &input, cv::Mat &output)
     // Create the output (same size and format as the input)
     output = cv::Mat::zeros(rows, cols, CV_8UC3);
 
+    // Get the default number of threads before modifying
+    int defaultThreads = omp_get_max_threads();
+
+    // No parallel => 1 thread only
+    if (!useParallel)
+        omp_set_num_threads(1); // Disable parallelism by using a single thread
+
+#pragma omp parallel for collapse(2) // Parallelize both y and x loops
     for (int y = 1; y < rows - 1; ++y)
     {
         for (int x = 1; x < cols - 1; ++x)
@@ -53,16 +69,15 @@ void FilterCPU(const cv::Mat &input, cv::Mat &output)
             { // R, G, B channels
                 float sum = 0.0f;
                 for (int ky = -1; ky <= 1; ++ky)
-                {
                     for (int kx = -1; kx <= 1; ++kx)
-                    {
                         sum += input.at<cv::Vec3b>(y + ky, x + kx)[c] * kernel.at<float>(ky + 1, kx + 1);
-                    }
-                }
                 output.at<cv::Vec3b>(y, x)[c] = cv::saturate_cast<uchar>(sum);
             }
         }
     }
+
+    // Restore the default threads
+    omp_set_num_threads(defaultThreads);
 }
 
 void FilterGPU(const cv::Mat &input, cv::Mat &output, GLFWwindow *window)
@@ -107,6 +122,58 @@ void FilterGPU(const cv::Mat &input, cv::Mat &output, GLFWwindow *window)
     fbo.Color_0().ToMat(output);
 }
 
+void RunSingle(const cv::Mat &original, GLFWwindow *window)
+{
+    cv::Mat outputCPU, outputGPU;
+
+    // FilterCPU(original, outputCPU, false);
+    FilterCPU(original, outputCPU, true);
+    FilterGPU(original, outputGPU, window);
+
+    // Show
+    cv::imshow("Output CPU", outputCPU);
+    cv::imshow("Output GPU", outputGPU);
+    cv::waitKey(0);
+}
+
+void RunBench(const cv::Mat &original, GLFWwindow *window)
+{
+    // Storage for the bench
+    std::vector<std::tuple<int, int, ms, ms, ms>> timings;
+
+    cv::Mat input, outputCPU, outputGPU;
+    std::vector<int> factors = {1, 2, 3, 4, 6, 8, 10};
+    for (int factor : factors)
+    {
+        cv::resize(original, input, cv::Size(factor * original.cols, factor * original.rows));
+        // Filter
+        auto t0 = std::chrono::high_resolution_clock::now();
+        FilterCPU(input, outputCPU, false);
+        auto t1 = std::chrono::high_resolution_clock::now();
+        FilterCPU(input, outputCPU, true);
+        auto t2 = std::chrono::high_resolution_clock::now();
+        FilterGPU(input, outputGPU, window);
+        auto t3 = std::chrono::high_resolution_clock::now();
+
+        auto durationCPU = toMS(t1 - t0);
+        auto durationCPU_MP = toMS(t2 - t1);
+        auto durationGPU = toMS(t3 - t2);
+
+        // timings
+        timings.push_back(std::make_tuple(factor, input.cols * input.rows, durationCPU, durationCPU_MP, durationGPU));
+    }
+
+    std::cout << "Factor\tInput\tCPU\tCPU_MP\tGPU" << std::endl;
+    for (auto &triplet : timings)
+    {
+        std::cout << std::get<0>(triplet) << "\t";
+        std::cout << std::get<1>(triplet) << "\t";
+        std::cout << std::get<2>(triplet).count() << "\t";
+        std::cout << std::get<3>(triplet).count() << "\t";
+        std::cout << std::get<4>(triplet).count() << std::endl;
+    }
+}
+
 int main()
 {
 
@@ -119,27 +186,11 @@ int main()
     if (glewInit() != GLEW_OK)
         throw std::runtime_error("glewInit failed");
 
-    // Load the input image
-    cv::Mat input = cv::imread("./res/montpellier.jpg");
-    cv::Mat outputCPU, outputGPU;
+    // Load the input image and create the containers
+    cv::Mat original = cv::imread("./res/montpellier.jpg");
 
-    // Filter
-    auto start = std::chrono::high_resolution_clock::now();
-    FilterCPU(input, outputCPU);
-    auto middle = std::chrono::high_resolution_clock::now();
-    FilterGPU(input, outputGPU, window);
-    auto end = std::chrono::high_resolution_clock::now();
-
-    auto durationCPU = std::chrono::duration_cast<std::chrono::milliseconds>(middle - start);
-    auto durationGPU = std::chrono::duration_cast<std::chrono::milliseconds>(end - middle);
-
-    std::cout << "CPU: " << durationCPU.count() << " ms" << std::endl;
-    std::cout << "GPU: " << durationGPU.count() << " ms" << std::endl;
-
-    // Show
-    cv::imshow("Output CPU", outputCPU);
-    // cv::imshow("Output GPU", outputGPU);
-    cv::waitKey(0);
+    RunSingle(original, window);
+    // RunBench(original, window);
 
     // Clean up
     glfwDestroyWindow(window);
